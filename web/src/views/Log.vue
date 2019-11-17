a499b7b0527327804f0b224179ba03a73a3764b7
bkleiner <b.kleiner@str8labs.com>
2019-11-17 13:17:03 +0100

    log: better vue key

--- web/src/views/Log.vue
+++ web/src/views/Log.vue
@@ -3,7 +3,7 @@
     <b-col sm="12">
       <b-card>
         <h5 slot="header" class="mb-0">Log</h5>
-        <samp v-for="line in log" :key="line">>> {{line}}</samp>
+        <samp v-for="(line, index) in log" :key="line+'-'+index">>> {{line}}</samp>
       </b-card>
     </b-col>
   </b-row>
