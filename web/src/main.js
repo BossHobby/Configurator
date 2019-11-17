e83bfd148c5001f1b6c4d9e42355727470a3ead3
bkleiner <b.kleiner@str8labs.com>
2019-11-17 14:17:22 +0100

    install sass-loader, add style.scss

--- web/src/main.js
+++ web/src/main.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import * as VueGL from "vue-gl";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './style.scss'
 import 'chartjs-plugin-crosshair'
 
 import App from './App.vue'
