beea850e995b638bdb6a79a98719372ac0a9c8f9
bkleiner <b.kleiner@str8labs.com>
2021-11-06 13:14:36 +0100

    update vue-gl and fix gyro model

--- web/src/components/GyroModel.vue
+++ web/src/components/GyroModel.vue
@@ -2,22 +2,44 @@
   <b-card>
     <h5 slot="header" class="mb-0">
       Model
-      <b-button size="sm" class="my-2 mx-2" @click="cal_imu()">calibrate</b-button>
+      <b-button size="sm" class="my-2 mx-2" @click="cal_imu()">
+        calibrate
+      </b-button>
     </h5>
-    <vgl-renderer camera="camera" scene="scene" style="width: 100%; height: 25vh" antialias>
-      <vgl-scene name="scene" background-color="#ffffff">
-        <vgl-box-geometry name="cube" width="7.5" height="1.5" depth="7.5"></vgl-box-geometry>
-        <vgl-mesh-standard-material name="std" color="#777"></vgl-mesh-standard-material>
-        <vgl-mesh geometry="cube" material="std" position="0 0 0" :rotation="rotation"></vgl-mesh>
-        <vgl-axes-helper size="140"></vgl-axes-helper>
-        <vgl-ambient-light color="#ffeecc"></vgl-ambient-light>
-        <vgl-directional-light position="0 1 1"></vgl-directional-light>
-      </vgl-scene>
+    <vgl-renderer style="width: 100%; height: 25vh" antialias>
+      <template #scene>
+        <vgl-scene background="#fff">
+          <vgl-mesh
+            :rotationX="rotation.x"
+            :rotationY="rotation.y"
+            :rotationZ="rotation.z"
+          >
+            <template #geometry>
+              <vgl-box-geometry :width="7.5" :height="1.5" :depth="7.5" />
+            </template>
+            <template #material>
+              <vgl-mesh-standard-material color="#777" />
+            </template>
+          </vgl-mesh>
 
-      <vgl-perspective-camera
-        name="camera"
-        :orbit-position="`15 ${0.35 * Math.PI} ${-0.5 * Math.PI}`"
-      ></vgl-perspective-camera>
+          <vgl-axes-helper :size="5"></vgl-axes-helper>
+          <vgl-ambient-light color="#ffeecc"></vgl-ambient-light>
+          <vgl-directional-light
+            :positionX="0"
+            :positionY="1"
+            :positionZ="1"
+          ></vgl-directional-light>
+        </vgl-scene>
+      </template>
+      <template #camera>
+        <vgl-perspective-camera
+          position="spherical"
+          rotation="lookAt"
+          :position-radius="15"
+          :position-phi="1"
+          :position-theta="0"
+        />
+      </template>
     </vgl-renderer>
   </b-card>
 </template>
@@ -29,22 +51,22 @@ export default {
   name: "GyroModel",
   computed: {
     ...mapState({
-      rate: state => state.profile.rate,
-      gyro_vector: state => state.state.GEstG
+      rate: (state) => state.profile.rate,
+      gyro_vector: (state) => state.state.GEstG,
     }),
     rotation() {
       const rot = this.gyro_vector;
       if (!rot) {
-        return "0 0 0 XYZ";
+        return { x: 0, y: 0, z: 0 };
       }
-      const x = Math.atan2(rot[0], rot[2]);
-      const y = Math.atan2(rot[1], rot[2]);
-      return `${x} 0 ${-y} XYZ`;
-    }
+      const x = Math.atan2(rot[1], rot[2]);
+      const y = Math.atan2(rot[0], rot[2]);
+      return { x: x, y: 0, z: y };
+    },
   },
   methods: {
-    ...mapActions(["cal_imu"])
-  }
+    ...mapActions(["cal_imu"]),
+  },
 };
 </script>
 
