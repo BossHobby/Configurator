b348f21ba04d2146af9e63a33e8f1713dd65c773
bkleiner <b.kleiner@str8labs.com>
2022-02-22 17:52:01 +0100

    add pwa test npm script

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,7 @@
-let publicPath = "/";
-
-if (process.env.NODE_ENV === 'production') {
-  if (process.env.DEPLOYMENT === 'gh-pages') {
-    publicPath = '/Configurator/';
-  } else {
-    publicPath = '/dist/';
-  }
-}
-
 process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
-  publicPath: publicPath,
+  publicPath: "/",
   pwa: {
     name: 'QUICKSILVER',
     themeColor: '#3c7317',
