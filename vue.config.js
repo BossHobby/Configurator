9ac166d082dfb600eaa6324f90047670b3d9c131
bkleiner <b.kleiner@str8labs.com>
2022-03-02 16:12:40 +0100

    fix public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,17 @@
+let publicPath = "/";
+
+if (process.env.NODE_ENV === 'production') {
+  if (process.env.DEPLOYMENT === 'gh-pages') {
+    publicPath = '/';
+  } else {
+    publicPath = '/dist/';
+  }
+}
+
 process.env.VUE_APP_VERSION = require('./package.json').version
 
 module.exports = {
-  publicPath: "/",
+  publicPath: publicPath,
   pwa: {
     name: 'QUICKSILVER',
     themeColor: '#3c7317',
