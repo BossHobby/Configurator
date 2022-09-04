8c6bedc745ab3b46f5ed66aefa43eeb31a3432aa
bkleiner <b.kleiner@str8labs.com>
2022-09-04 17:14:45 +0200

    add version check for vtx

--- src/panel/VTX.vue
+++ src/panel/VTX.vue
@@ -114,7 +114,10 @@
     </div>
 
     <footer class="card-footer">
-      <spinner-btn class="card-footer-item" @click="vtx.apply_vtx_settings(vtx.settings)">
+      <spinner-btn
+        class="card-footer-item"
+        @click="vtx.apply_vtx_settings(vtx.settings)"
+      >
         Apply
       </spinner-btn>
     </footer>
@@ -124,27 +127,68 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useVTXStore } from "@/store/vtx";
+import { useInfoStore } from "@/store/info";
 
 export default defineComponent({
   name: "vtx",
+  setup() {
+    return {
+      vtx: useVTXStore(),
+      info: useInfoStore(),
+    };
+  },
   data() {
     return {
       protocolNames: ["INVALID", "TRAMP", "SMARTAUDIO", "MSP_VTX"],
-      vtxProtocolOptions: [
+      frequencyTable: [
+        [5865, 5845, 5825, 5805, 5785, 5765, 5745, 5725],
+        [5733, 5752, 5771, 5790, 5809, 5828, 5847, 5866],
+        [5705, 5685, 5665, 5645, 5885, 5905, 5925, 5945],
+        [5740, 5760, 5780, 5800, 5820, 5840, 5860, 5880],
+        [5658, 5695, 5732, 5769, 5806, 5843, 5880, 5917],
+        [5333, 5373, 5413, 5453, 5493, 5533, 5573, 5613],
+      ],
+    };
+  },
+  computed: {
+    vtxProtocolOptions() {
+      const data = [
         { value: 0, text: "AUTO" },
         { value: 1, text: "TRAMP" },
         { value: 2, text: "SMARTAUDIO" },
-        { value: 3, text: "MSP_VTX" },
-      ],
-      vtxBandOptions: [
+      ];
+      if (this.info.quicVersionGt("0.1.1")) {
+        data.push({ value: 3, text: "MSP_VTX" });
+      }
+      return data;
+    },
+    vtxBandOptions() {
+      const data = [
         { value: 0, text: "VTX_BAND_A" },
         { value: 1, text: "VTX_BAND_B" },
         { value: 2, text: "VTX_BAND_E" },
         { value: 3, text: "VTX_BAND_F" },
         { value: 4, text: "VTX_BAND_R" },
-        { value: 5, text: "VTX_BAND_L" },
-      ],
-      vtxChannelOptions: [
+      ];
+      if (this.info.quicVersionGt("0.1.1")) {
+        data.push({ value: 5, text: "VTX_BAND_L" });
+      }
+      return data;
+    },
+    vtxPowerLevelOptions() {
+      const data = [
+        { value: 0, text: "VTX_POWER_LEVEL_1" },
+        { value: 1, text: "VTX_POWER_LEVEL_2" },
+        { value: 2, text: "VTX_POWER_LEVEL_3" },
+        { value: 3, text: "VTX_POWER_LEVEL_4" },
+      ];
+      if (this.info.quicVersionGt("0.1.1")) {
+        data.push({ value: 4, text: "VTX_POWER_LEVEL_5" });
+      }
+      return data;
+    },
+    vtxChannelOptions() {
+      return [
         { value: 0, text: "VTX_CHANNEL_1" },
         { value: 1, text: "VTX_CHANNEL_2" },
         { value: 2, text: "VTX_CHANNEL_3" },
@@ -153,32 +197,15 @@ export default defineComponent({
         { value: 5, text: "VTX_CHANNEL_6" },
         { value: 6, text: "VTX_CHANNEL_7" },
         { value: 7, text: "VTX_CHANNEL_8" },
-      ],
-      vtxPitModeOptions: [
+      ];
+    },
+    vtxPitModeOptions() {
+      return [
         { value: 0, text: "Off" },
         { value: 1, text: "On" },
         // { value: 2, text: "NO SUPPORT" }
-      ],
-      vtxPowerLevelOptions: [
-        { value: 0, text: "VTX_POWER_LEVEL_1" },
-        { value: 1, text: "VTX_POWER_LEVEL_2" },
-        { value: 2, text: "VTX_POWER_LEVEL_3" },
-        { value: 3, text: "VTX_POWER_LEVEL_4" },
-        { value: 4, text: "VTX_POWER_LEVEL_5" },
-      ],
-      frequencyTable: [
-        [5865, 5845, 5825, 5805, 5785, 5765, 5745, 5725],
-        [5733, 5752, 5771, 5790, 5809, 5828, 5847, 5866],
-        [5705, 5685, 5665, 5645, 5885, 5905, 5925, 5945],
-        [5740, 5760, 5780, 5800, 5820, 5840, 5860, 5880],
-        [5658, 5695, 5732, 5769, 5806, 5843, 5880, 5917],
-      ],
-    };
-  },
-  setup() {
-    return {
-      vtx: useVTXStore(),
-    };
+      ];
+    },
   },
 });
 </script>
