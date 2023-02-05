1c6e949df5e9101245d74620b7f77e9a340c022d
bkleiner <b.kleiner@str8labs.com>
2023-02-05 17:07:24 +0100

    template: rework to media element

--- src/views/Templates.vue
+++ src/views/Templates.vue
@@ -1,59 +1,27 @@
 <template>
-  <div class="columns is-multiline">
-    <div
-      class="column mb-3 is-one-third"
-      v-for="tmpl of templates.index"
-      :key="tmpl.name"
-    >
-      <div class="card template-card">
-        <div class="card-image">
-          <figure class="image is-square">
-            <img :src="tmpl.image" />
-          </figure>
-        </div>
-        <div class="card-content">
-          <p class="title">
-            {{ tmpl.name }}
-          </p>
-          <p class="subtitle">
-            {{ tmpl.category }}
-          </p>
-          <div class="content">
-            {{ tmpl.desc }}
-          </div>
-        </div>
-        <footer class="card-footer">
-          <span class="card-footer-item"></span>
-          <spinner-btn
-            class="card-footer-item is-primary"
-            @click="applyProfile(tmpl)"
-          >
-            Apply
-          </spinner-btn>
-        </footer>
-      </div>
-    </div>
-  </div>
+  <TemplateCard
+    v-for="tmpl of templates.index"
+    :key="tmpl.name"
+    :template="tmpl"
+  ></TemplateCard>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useTemplatesStore } from "@/store/templates";
 import { useProfileStore } from "@/store/profile";
-import TemplateModal from "@/components/TemplateModal.vue";
+import TemplateCard from "@/components/TemplateCard.vue";
 
 export default defineComponent({
   name: "Templates",
+  components: {
+    TemplateCard,
+  },
   setup() {
     return {
       templates: useTemplatesStore(),
       profile: useProfileStore(),
     };
-  },
-  methods: {
-    applyProfile(p) {
-      this.$modal.show(TemplateModal, { template: p });
-    },
   },
   created() {
     this.templates.fetch_templates();
@@ -63,9 +31,8 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .template-card {
-  max-width: 18em;
-
   .card-content {
+    height: 150px;
     overflow: hidden;
     text-overflow: ellipsis;
   }
