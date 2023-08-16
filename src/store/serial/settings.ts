fdcaf941b4bb6fe23a5e1ee4385c792ed8d7eb6c
kikoqiu <kikoqiu@163.com>
2023-08-16 14:37:00 +0800

    use a modified version of web-serial-polyfill to fix error of zero-length chunk exception for the method pull

--- src/store/serial/settings.ts
+++ src/store/serial/settings.ts
@@ -2,7 +2,7 @@ const isAndroid = /(android)/i.test(navigator.userAgent);
 
 const androidSerialSettings = {
   baudRate: 921600,
-  bufferSize: 64,
+  bufferSize: 4 * 1024 * 1024,
   updateInterval: 1000,
 };
 
