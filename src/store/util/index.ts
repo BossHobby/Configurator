2043375a3a9c9beb778ecbb5fbb171ad0afa567d
bkleiner <b.kleiner@str8labs.com>
2023-10-22 02:06:48 +0200

    remove unused function

--- src/store/util/index.ts
+++ src/store/util/index.ts
@@ -7,13 +7,6 @@ export function concatUint8Array(a: Uint8Array, b: Uint8Array): Uint8Array {
   return res;
 }
 
-export function concatArrayBuffer(a: Uint8Array, b: Uint8Array): Uint8Array {
-  const res = new Uint8Array(a.length + b.length);
-  res.set(a);
-  res.set(b, a.length);
-  return res;
-}
-
 export function stringToUint8Array(str: string): Uint8Array {
   const res = new Uint8Array(str.length);
   for (let i = 0; i < str.length; i++) {
