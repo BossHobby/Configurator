f86a1ad717f16aef69862e7231acb7f1acf97ed8
bkleiner <b.kleiner@str8labs.com>
2024-05-04 13:52:44 +0200

    remove @vue/compat

--- env.d.ts
+++ env.d.ts
@@ -3,15 +3,6 @@ import { ElectronAPI } from "@electron-toolkit/preload";
 /// <reference types="vite/client" />
 /// <reference types="vite-svg-loader" />
 
-declare module "vue" {
-  import { CompatVue } from "@vue/runtime-dom";
-  const Vue: CompatVue;
-  export default Vue;
-  export * from "@vue/runtime-dom";
-  const { configureCompat } = Vue;
-  export { configureCompat };
-}
-
 declare global {
   interface Window {
     electron?: ElectronAPI;
