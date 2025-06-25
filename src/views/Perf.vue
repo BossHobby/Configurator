cb84a44a72ff1a19878379a877501891c53a7179
bkleiner <benedikt.kleiner@gmail.com>
2025-06-25 23:15:48 +0200

    perf: display total time

--- src/views/Perf.vue
+++ src/views/Perf.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="columns is-multiline">
+    <div v-if="totalTaskPerf" class="column is-12 my-3">
+      <RealtimePlot
+        :title="'Total Task Performance'"
+        :axis="Object.keys(totalTaskPerf).slice(1)"
+        :input="totalTaskPerf"
+        class="image is-fullwidth is-4by3"
+      ></RealtimePlot>
+    </div>
     <div
       v-for="(counter, index) in perf.counters"
       :key="'counter' + index"
@@ -16,7 +24,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
 import RealtimePlot from "@/components/RealtimePlot.vue";
 import { usePerfStore } from "@/store/perf";
 
@@ -26,8 +34,49 @@ export default defineComponent({
     RealtimePlot,
   },
   setup() {
+    const perf = usePerfStore();
+
+    const totalTaskPerf = computed(() => {
+      if (!perf.counters || perf.counters.length === 0) {
+        return null;
+      }
+
+      // Create an object to store the summed values
+      const total = {
+        name: "Total Task Performance",
+      };
+
+      // Get the keys from the first counter (excluding 'name')
+      const firstCounter = perf.counters[0];
+      const valueKeys = Object.keys(firstCounter).filter(
+        (key) => key !== "name",
+      );
+
+      // Initialize all value keys to 0
+      valueKeys.forEach((key) => {
+        total[key] = 0;
+      });
+
+      // Sum up all counter values except USB task
+      perf.counters.forEach((counter) => {
+        // Skip USB task
+        if (counter.name && counter.name.toLowerCase().includes("usb")) {
+          return;
+        }
+
+        valueKeys.forEach((key) => {
+          if (typeof counter[key] === "number") {
+            total[key] += counter[key];
+          }
+        });
+      });
+
+      return total;
+    });
+
     return {
-      perf: usePerfStore(),
+      perf,
+      totalTaskPerf,
     };
   },
 });
