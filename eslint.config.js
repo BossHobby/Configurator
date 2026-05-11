ee0bbe5cbdc16c62d50f58adbe2cb18ecb19e89a
Benedikt Kleiner <benedikt.kleiner@tbs-avionics.com>
2026-05-11 13:30:03 +0800

    chore(deps): update configurator dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,22 @@
 import pluginVue from "eslint-plugin-vue";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
   ...pluginVue.configs["flat/recommended"],
   eslintPluginPrettierRecommended,
   {
+    languageOptions: {
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
     rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
+      "vue/multi-word-component-names": "off",
+      "vue/no-unused-components": "off",
+      "vue/require-toggle-inside-transition": "off",
+      "vue/require-v-for-key": "off",
+      "vue/valid-v-model": "off",
     },
   },
 ];
