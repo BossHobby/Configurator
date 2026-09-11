5a7472e834748163a12b576dba292b3715a5a120
Benedikt Kleiner <benedikt.kleiner@tbs-avionics.com>
2026-09-11 15:43:16 +0800

    build: minify Electron renderer for service worker precache

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
   renderer: {
     root: ".",
     build: {
+      minify: true,
       rollupOptions: {
         input: {
           index: resolve(__dirname, "index.html"),
